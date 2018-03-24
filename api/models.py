556f11c009cf03ef7395e2f05af98c9d0a2c782b
Cyan Tarek <cyantarek.cg@gmail.com>
2018-03-24 21:30:57 +0600

    Add files via upload

---
+++ api/models.py
@@ -0,0 +1,4 @@
+from django.db import models #for sql
+from djongo import models as m_models #for mongodb nosql
+
+# TODO: write here your models
---
+++ api/views.py
@@ -0,0 +1,3 @@
+from rest_framework.views import APIView
+
+# TODO: add here your API Views
